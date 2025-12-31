04b63d2562083947604c1e9a81f678ccd2745a13
Salih Gungor <salih@gmail.com>
2025-12-31 11:23:21 +0300

    pdf-satis

--- server.js
+++ server.js
@@ -29,6 +29,11 @@ import fs from "fs";
 import path from "path";
 import cors from "cors";
 import multer from "multer";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
